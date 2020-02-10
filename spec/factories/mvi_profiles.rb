6576ab3a7577bd6269de52e9ff2826966b12f1ab
Kam Karshenas <kam@adhocteam.us>
2020-02-10 11:18:09 -0800

    dont use faker for these

--- spec/factories/mvi_profiles.rb
+++ spec/factories/mvi_profiles.rb
@@ -40,9 +40,9 @@ FactoryBot.define do
     given_names { Array.new(2) { Faker::Name.first_name } }
     family_name { Faker::Name.last_name }
     suffix { Faker::Name.suffix }
-    gender { Faker::Medical::Patient.gender }
+    gender { %w[M F].sample }
     birth_date { Faker::Date.between(from: 80.years.ago, to: 30.years.ago).strftime('%Y%m%d') }
-    ssn { Faker::Medical::SSN.ssn.delete('-') }
+    ssn { ((1..1000).to_a.shuffle.join.to_s)[0, 9] }
     address { build(:mvi_profile_address) }
     home_phone { Faker::PhoneNumber.phone_number }
     full_mvi_ids {
